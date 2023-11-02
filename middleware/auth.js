dea794079a448bb8d084ee622c86d1ab43b6635d
Zejdi1 <zl51041@utb-uni.net>
2023-11-02 16:51:53 +0100

    Merge branch 'testbranch'

--- middleware/auth.js
+++ middleware/auth.js
@@ -17,63 +17,12 @@ const verifyToken = async (req, res, next) => {
     };
 
     next();
-  } catch (err) {
-    return res.status(500).json({ error: err.message });
+  } catch (error) {
+    console.log(error);
+    next("Authentication failed");
   }
 };
 
 
 export default verifyToken;
 
-
-  console.log("DB Connected");
-    const refreshTokens = [];
-
-    // Middleware to verify access token
-    function authenticateAccessToken(req, res, next) {
-      const authHeader = req.headers['authorization'];
-      const token = authHeader && authHeader.split(' ')[1];
-
-      if (!token) {
-        return res.sendStatus(401);
-      }
-
-      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        if (err) {
-          return res.sendStatus(403);
-        }
-        req.user = user;
-        next();
-      });
-    }
-
-    app.post('/login', (req, res) => {
-
-      const username = req.body.username;
-      // Check the user's credentials here and, if valid, generate tokens
-      // ...
-
-      if (username) {
-        const user = { name: username };
-        const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '15m' });
-        const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
-
-        // Store the refresh token in your database
-        refreshTokens.push(refreshToken);
-
-        res.json({ accessToken, refreshToken });
-      } else {
-        res.status(401).json({ error: 'Authentication failed' });
-      }
-    });
-
-    // Your protected route using the authenticateAccessToken middleware
-    app.get('/protected', authenticateAccessToken, (req, res) => {
-      // Access token is verified, and you can access the user data via req.user
-      res.json({ message: 'Protected data', user: req.user });
-    });
-    app.post("/auth/register", register);
-    app.use("/auth", authRoutes);
-    app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
-
-  
