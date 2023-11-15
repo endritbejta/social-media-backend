1063ac20cb6a675c7e7d7c0fb3ccf9ab0a37ab6d
Zejdi1 <zl51041@utb-uni.net>
2023-11-15 13:19:12 +0100

    change

--- models/notification.js
+++ models/notification.js
@@ -5,21 +5,18 @@ import Notification from "../models/notificationModel.js";
 
   export default async function createLikeNotification(userId, postId, postOwnerId) {
   try {
-    // Create a new notification
+   
     const newNotification = new Notification({
-      userId: postOwnerId, // User who should receive the notification
-      message:`${userId} liked your post` , // Customize the notification message
-      postId: postId, // ID of the post being liked
-      type: 'like', // You can add more types for different actions
-      read: false, // Notification is initially unread
+      userId: postOwnerId,
+      message:`${userId} liked your post` , 
+      postId: postId, 
+      type: 'like', 
+      read: false, 
     });
 
-    // Save the notification to the database
     await newNotification.save();
 
     console.log('Notification created:', newNotification);
-
-    // You can also emit a socket.io event or use a message queue to notify the user in real-time
 
     return newNotification;
   } catch (error) {
@@ -31,21 +28,17 @@ import Notification from "../models/notificationModel.js";
 
 export  async function createCommentNotification(userId, postId, postOwnerId) {
   try {
-    // Create a new notification
     const newNotification = new Notification({
-      userId: postOwnerId, // User who should receive the notification
-      message:`${userId} coment your post` , // Customize the notification message
-      postId: postId, // ID of the post being liked
-      type: 'like', // You can add more types for different actions
-      read: false, // Notification is initially unread
+      userId: postOwnerId, 
+      message:`${userId} comment your post` , 
+      postId: postId, 
+      type: 'like', 
+      read: false, 
     });
 
-    // Save the notification to the database
     await newNotification.save();
 
     console.log('Notification created:', newNotification);
-
-    // You can also emit a socket.io event or use a message queue to notify the user in real-time
 
     return newNotification;
   } catch (error) {
