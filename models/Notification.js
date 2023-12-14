74a6c2d3a19638ae1defdb2d2406f905c6696171
Zejdi1 <zl51041@utb-uni.net>
2023-12-14 22:00:39 +0100

    Created Notification.js

--- models/Notification.js
+++ models/Notification.js
@@ -1,58 +1,138 @@
+import mongoose from "mongoose";
+import Notification from "../models/notificationModel.js";
 
-var admin = require("firebase-admin");
-var FCM = require("fcm-notification");
-
-var serviceAccount = require("./serviceAccountKey.json");
-const certPath = admin.credential.cert(serviceAccount);
-var FCMInstance = new FCM(certPath);
-
-// Function to send a notification
-function sendNotification(title, message, token, res) {
-    let notification = {
-        notification: {
-            title: title,
-            body: message
-        },
-        data: {
-            orderId: "123456",
-            orderDate: "2023-10-18"
-        },
-        token: token
-    };
-
-    FCMInstance.send(notification, function (err, response) {
-        if (err) {
-            console.error("Error sending notification:", err);
-            return res.status(500).send({
-                message: err
-            });
-        } else {
-            return res.status(200).send({
-                message: "Notification sent successfully"
-            });
-        }
+// Function to create a like notification
+export default async function createLikeNotification(userId, postId, postOwnerId) {
+  try {
+    const newNotification = new Notification({
+      userId: postOwnerId,
+      message: `${userId} liked your post`,
+      postId: postId,
+      type: 'like',
+      read: false,
     });
+
+    await newNotification.save();
+
+    return newNotification;
+  } catch (error) {
+    console.error('Error creating like notification:', error);
+    throw error;
+  }
 }
+export async function createCommentNotification(userId, postId, postOwnerId) {
+  try {
+    const newNotification = new Notification({
+      userId: postOwnerId,
+      message: `${userId} commented on your post`,
+      postId: postId,
+      type: 'comment',
+      read: false,
+    });
 
-exports.Notification = (req, res, next) => {
-    try {
-        sendNotification("Test Notification", "Notification Message", req.body.fcm_token, res);
-    } catch (err) {
-        console.error(err);
-        res.status(500).send({
-            message: "Server error"
-        });
-    }
-};
+    await newNotification.save();
 
-exports.sendNotification = async (req, res) => {
-    try {
-        const { notificationTitle, notificationMessage, token } = req.body;
-        sendNotification(notificationTitle, notificationMessage, token, res);
-    } catch (err) {
-        console.error(err);
-        res.status(500).send({
-            message: "Server error"
-        });
-    }
-};
+    console.log('Comment Notification created:', newNotification);
+
+    // You can fire socket.io events or use a message queue for real-time notifications
+
+    return newNotification;
+  } catch (error) {
+    console.error('Error creating comment notification:', error);
+    throw error;
+  }
+}
+// // Function to create a comment notification
+// export async function createCommentNotification(userId, postId, postOwnerId) {
+//   try {
+//     const newNotification = new Notification({
+//       userId: postOwnerId,
+//       message: `${userId} commented on your post`,
+//       postId: postId,
+//       type: 'comment',
+//       read: false,
+//     });
+
+//     await newNotification.save();
+
+//     console.log('Comment Notification created:', newNotification);
+
+//     // You can fire socket.io events or use a message queue for real-time notifications
+
+//     return newNotification;
+//   } catch (error) {
+//     console.error('Error creating comment notification:', error);
+//     throw error;
+//   }
+// }
+
+// // Function to create a share notification
+// export async function createShareNotification(userId, postId, postOwnerId) {
+//   try {
+//     const newNotification = new Notification({
+//       userId: postOwnerId,
+//       message: `${userId} shared your post`,
+//       postId: postId,
+//       type: 'share',
+//       read: false,
+//     });
+
+//     await newNotification.save();
+
+//     console.log('Share Notification created:', newNotification);
+
+//     // You can fire socket.io events or use a message queue for real-time notifications
+
+//     return newNotification;
+//   } catch (error) {
+//     console.error('Error creating share notification:', error);
+//     throw error;
+//   }
+// }
+
+// // Function to create a friend request notification
+// export async function createFriendRequestNotification(senderId, recipientId) {
+//   try {
+//     const newNotification = new Notification({
+//       userId: recipientId,
+//       message: `${senderId} sent you a friend request`,
+//       type: 'friendRequest',
+//       read: false,
+//     });
+
+//     await newNotification.save();
+
+//     console.log('Friend Request Notification created:', newNotification);
+
+//     // You can fire socket.io events or use a message queue for real-time notifications
+
+//     return newNotification;
+//   } catch (error) {
+//     console.error('Error creating friend request notification:', error);
+//     throw error;
+//   }
+// }
+
+// // Function to create a post notification
+// export async function createPostNotification(userId, postId, postOwnerId) {
+//   try {
+//     const newNotification = new Notification({
+//       userId: postOwnerId,
+//       message: `${userId} created a new post`,
+//       postId: postId,
+//       type: 'post',
+//       read: false,
+//     });
+
+//     await newNotification.save();
+
+//     console.log('Post Notification created:', newNotification);
+
+//     // You can fire socket.io events or use a message queue for real-time notifications
+
+//     return newNotification;
+//   } catch (error) {
+//     console.error('Error creating post notification:', error);
+//     throw error;
+//   }
+// }
